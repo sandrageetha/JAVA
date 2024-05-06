302d688ec904e01bf607d7fb86881169d48a3682
sandrageetha <sandrageetha02@gmail.com>
2024-05-07 02:11:48 +0530

    true or false

---
+++ ifelse.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String[] args){
+        int num =8;
+        if(num>10)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
