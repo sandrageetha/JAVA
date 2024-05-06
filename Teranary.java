436de88ba807d45a264a72086223c9e1d5edbb13
sandrageetha <sandrageetha02@gmail.com>
2024-05-07 02:41:20 +0530

    even/odd

---
+++ Teranary.java
@@ -0,0 +1,9 @@
+public class Teranary{
+    public static void main(String[] args){
+        //instead of if else and result stored
+        int num =8;
+        int result =0;
+        result = num%2==0 ? 10 :20;
+        System.out.println(result);
+    }
+}
