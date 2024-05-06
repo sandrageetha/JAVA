8dba0b7a868279b42c0190bda5636c69cd5d1deb
sandrageetha <sandrageetha02@gmail.com>
2024-05-07 02:52:56 +0530

    Days in a week

---
+++ Switch.java
@@ -0,0 +1,28 @@
+public class Switch{
+    public static void main(String[] args){
+        int n = 6;
+        switch(n){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("out of days");
+
+        }
+    }
+} 
