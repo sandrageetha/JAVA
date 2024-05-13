1db31c7a38b091914eeadb1af71cdfd20b1cd50b
sandrageetha <sandrageetha02@gmail.com>
2024-05-13 11:01:17 +0530

    modified Challenge Number :1

--- challengeOne.java
+++ challengeOne.java
@@ -1,12 +1,13 @@
 public class challengeOne{
     public static void main(String[] args){
-        double firstVar = 20;
-        double secondVar = 80;
-        double resultVar = (firstVar+secondVar)*100;
-        double remainder = (resultVar % 40.00);
-        boolean result = (remainder == 0.00)?true : false;
+        double firstVar = 20.00d;
+        double secondVar = 80.00d;
+        double resultVar = (firstVar+secondVar)* 100.00d;
+        double remainder = (resultVar % 40.00d);
+        System.out.println(remainder);
+        boolean result = (remainder == 0)? true : false;
         System.out.println(result);
-        if(result == false){
+        if(!result){
             System.out.println("got some remainder");
         }
     }
