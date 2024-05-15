39f31657bc81c912f0a9d44a2651bd2a5580107f
sandrageetha <sandrageetha02@gmail.com>
2024-05-15 17:07:38 +0530

    Leap year

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if (year>=1 && year<=9999){
+            if((year %4 == 0) &&( year % 100==0)  &&(year % 400 ==0) ){
+                return true;
+                }
+            else if((year %4 == 0) &&( year % 100!=0)) {
+                return true;
+                }
+            else{
+            return false;
+        }
+        }
+        else{
+            return false;
+        }
+
+}
+public static void main(String[] args){
+    isLeapYear(1600);
+}
+}
